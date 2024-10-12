29d062e2ad01737cfe07be2a636b4d34201da291
asheshjyotii <asheshjyotimajumdar3@gmail.com>
2024-10-12 18:39:36 +0530

    switch case choices

--- main.py
+++ main.py
@@ -1,24 +1,52 @@
 '''
 Todo:
-create list object
-infinite take user input
-prit user input in first letter capitals
-add the user input into the list
+switch statement
+add 
+show
+edit
+exit
 '''
+
+todos = []
+
 service = True
-todos =[]
-todo = input("Enter First Todo:\n")
-todo = todo.capitalize()
-print('The entered todo is:',todo)
-todos.append(todo)  
+
 while service:
-    todo = input("Enter next Todo or 'exit' to Exit:\n")
-    if todo == 'exit':
-        service = False
-    else:
-        todo = todo.capitalize()
-        print('The entered todo is:',todo)
-        todos.append(todo)
-print("The todos are:")
-for x in todos:
-    print(x)
+    userInput = input('''Available Options:
+    add -> to add a todo
+    show -> to show todo list
+    edit -> to edit any particular todo
+    exit -> to show the todo list & exit the app\n-> ''')
+    userInput = userInput.strip(" ")
+    match userInput:
+        case 'add':
+            userInput = input("Enter the todo: ")
+            todo = userInput.capitalize()
+            todos.append(todo)
+            print(f"Added '{todo}' to the list...")
+        case 'show':
+            print("The todo List:")
+            for index,x in enumerate(todos):
+                print(f"{index+1}.{x}")
+        case 'edit':
+            for index,x in enumerate(todos):
+                print(f"Enter {index+1} to change {x}")
+
+            userInput = int(input("-> "))
+            print("length: ",len(todos))
+            for x in todos:
+                print(x)
+            if userInput <=0 or userInput > len(todos):
+                print("Invalid choice.")
+                continue
+
+            newTodo = input("Enter the new todo: ")
+            todos [userInput-1] = newTodo
+
+            print("The new todo list:")
+            for x in todos:
+                print(x)
+        case 'exit':
+            for x in todos:
+                print(x)
+            break
