eedf38da5122ee9086b8adbddb3c4745f0cb86ec
asheshjyotii <asheshjyotimajumdar3@gmail.com>
2025-04-26 23:17:35 +0530

    handled empty todo input

--- webapp/src/main.py
+++ webapp/src/main.py
@@ -1,18 +1,34 @@
 import streamlit as st
+import datetime
 import utils as ut
 todos = ut.read_todos()
 def empty():
     pass
 def new_todo():
     todo = st.session_state["new_todo"] + '\n'
-    todos.append(todo)
-    ut.write_todos(todos)
+    if todo != "\n":
+        print(type(todo))
+        todos.append(todo)
+        ut.write_todos(todos)
+    else:
+        print("empty")
+        pass
 
 todos = ut.read_todos() #becomes a list of todos read from the todos file
 
-st.title("Todo App")
-st.subheader("This is my simple todo list app")
-st.write("I hope this can increase my productivity...")
+st.title("📝 Your Personal Todo Manager")
+st.subheader("Stay Organized, Stay Focused!")
+
+st.divider()
+
+today = datetime.date.today()
+st.write(f"📅 **Today:** {today.strftime('%A, %B %d, %Y')}")
+
+st.success("Ready to conquer your tasks today? Let's go! 💪")
+
+st.write("Add, track, and complete your daily tasks effortlessly 🚀")
+
+
 
 for index, todo in enumerate(todos):
     checkbox_status = st.checkbox(todo, key=todo+str(index))
@@ -24,4 +40,3 @@ for index, todo in enumerate(todos):
 
 st.text_input(label="Add new Todo",placeholder="Enter a todo....", label_visibility="hidden",on_change= new_todo, key="new_todo" )
 
-st.session_state
